9463e062cd806be2135bf08bcf448a4dd8bb774f
yenniespinoza <93500177+yenniespinoza@users.noreply.github.com>
2021-11-23 22:13:48 -0600

    Update L4_base.py

--- L4_base.py
+++ L4_base.py
@@ -1,35 +1,39 @@
-# Base para la solución del Laboratorio 4
-
 # Los parámetros T, t_final y N son elegidos arbitrariamente
 
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-# Variables aleatorias A y Z
-t = np.linspace(0, 0.1, 100)
+# Parte a) con Omega (la frecuencia) constante
+W = 2*np.pi*60
+
+# Variables aleatorias C, T y Z
+vaC = stats.norm(5, np.sqrt(0.2))
+vaZ = stats.uniform(0, np.pi/2)
+
+# Creación del vector de tiempo
+T = 100			# número de elementos
+t_final = 10	# tiempo en segundos
+t = np.linspace(0, t_final, T)
 
 # Inicialización del proceso aleatorio X(t) con N realizaciones
-A = [1,-1]  # Se tiene dos posibles valores para la amplitud
-N = 4       # Se trabajará con 4 posibilidades de la onda
+N = 10
 X_t = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
 
-# Creación de las muestras del proceso x(t) (A y Z independientes)
-    # A continuación, para la señal se prueba cada valor de amplitud
+# Creación de las muestras del proceso x(t) (C Z independientes)
 for i in range(N):
-	x1 = -i*np.cos(2*np.pi*fc*t)+i*np.sin(2*np.pi*fc*t)
-	x2 = i*np.cos(2*np.pi*fc*t)+i*np.sin(2*np.pi*fc*t)
-	X_t[i,:] = x1
-	X_t[i+1,:] = x2
-	plt.plot(t, x1, lw=2)
-	plt.plot(t, x2, lw=2)
+	C = vaC.rvs()
+	Z = vaZ.rvs()
+	x_t = C * np.cos(W*t + Z)
+	X_t[i,:] = x_t
+	plt.plot(t, x_t)
 
 # Promedio de las N realizaciones en cada instante (cada punto en t)
 P = [np.mean(X_t[:,i]) for i in range(len(t))]
 plt.plot(t, P, lw=6)
 
 # Graficar el resultado teórico del valor esperado
-E = np.mean(senal_Tx)*t
+E = 10/np.pi *( np.cos(W*t) - np.sin(W*t))
 plt.plot(t, E, '-.', lw=4)
 
 # Mostrar las realizaciones, y su promedio calculado y teórico
@@ -38,29 +42,45 @@ plt.xlabel('$t$')
 plt.ylabel('$x_i(t)$')
 plt.show()
 
+# Parte b) con Omega (la frecuencia) y Theta (Z) constante		    
+Theta = np.pi/2		       
+	       
+# Inicialización del proceso aleatorio X(t) con N realizaciones
+	       
+X_t2 = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
+
+for i in range(N):
+	C2 = vaC.rvs()
+	x_t2 = C2 * np.cos(W*t + Theta)
+	X_t2[i,:] = x_t2	       
+
+# Promedio de las N realizaciones en cada instante (cada punto en t)
+P2 = [np.mean(X_t2[:,i]) for i in range(len(t))]
+	       
 # T valores de desplazamiento tau
-desplazamiento = np.arange(T)
-taus = desplazamiento/t_final
+desplazamiento2 = np.arange(T)
+taus2 = desplazamiento2/t_final	       
+
 
 # Inicialización de matriz de valores de correlación para las N funciones
-corr = np.empty((N, len(desplazamiento)))
+corr2 = np.empty((N, len(desplazamiento2)))
 
 # Nueva figura para la autocorrelación
 plt.figure()
 
 # Cálculo de correlación para cada valor de tau
 for n in range(N):
-	for i, tau in enumerate(desplazamiento):
-		corr[n, i] = np.correlate(X_t[n,:], np.roll(X_t[n,:], tau))/T
-	plt.plot(taus, corr[n,:])
+	for i, tau in enumerate(desplazamiento2):
+		corr2[n, i] = np.correlate(X_t2[n,:], np.roll(X_t2[n,:], tau))/T
+	plt.plot(taus2, corr2[n,:])
 
 # Valor teórico de correlación
-Rxx = 19/2 * np.cos(np.pi*taus)
+Rxx2 = 25.2 * np.cos(W*t+Theta)*np.cos(W*(t+taus2)+Theta)
 
 # Gráficas de correlación para cada realización y la
-plt.plot(taus, Rxx, '-.', lw=4, label='Correlación teórica')
+plt.plot(taus2, Rxx2, '-.', lw=4, label='Correlación teórica')
 plt.title('Funciones de autocorrelación de las realizaciones del proceso')
 plt.xlabel(r'$\tau$')
 plt.ylabel(r'$R_{XX}(\tau)$')
 plt.legend()
-plt.show()
+plt.show()  	    
